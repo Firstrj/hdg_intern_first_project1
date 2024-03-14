3926fc889a6c79b1633686c698b69398c7101c47
siriake <siri.ake.1980@gmail.com>
2024-03-14 20:09:59 +0700

    122

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.use(express.json());
 
 app.use('/api/product', require('./routes/productRoutes'));
 
-mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://siriake1980:rdKEnjUTBRDusYqs@cluster0.d5wpcwv.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
   .catch(err => console.error('Could not connect to MongoDB', err));
 
 
-const PORT = 3000;
+const PORT = 3137;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
